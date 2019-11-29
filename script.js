7a10040fa7470e14e8e34b8916e992094a5432f7
Anshuman <56269970+i-anshuman@users.noreply.github.com>
2019-11-29 23:21:39 +0530

    script.js
    Bug fixed. No it shows the arrow down icon.

--- script.js
+++ script.js
@@ -17,37 +17,46 @@ function addTask(e) {
 
   // Create new li element
   var li = document.createElement("li");
+
   // Add class
   li.className = "task-item";
 
   // Add text node with input value
   li.appendChild(document.createTextNode(newTask));
 
-  // Create del button element
+  
+  // Create an arrow down Element or <span> element that hold the icon (arrow down)
+  var spanElement = document.createElement("span");
 
+  // Create an <i> element.
+  var iElement = document.createElement("i");
+
+  // Add material icon class to it
+  iElement.className = "material-icons";
+
+  // Add content to <i> tag so that it can dispaly icon
+  iElement.innerHTML = "keyboard_arrow_down";
+
+  // Add <i> element to <span> element
+  spanElement.appendChild(iElement);
+
+
+  // Append to li elements
+  li.appendChild(spanElement);
+
+
+  // Moved the del button to bottom so that it can be appended after <span>
+  // Create del button element
   var deleteBtn = document.createElement("button");
 
   // ADD Class to del button
   deleteBtn.className = "btn-danger";
 
   // Append text node
-
   deleteBtn.appendChild(document.createTextNode("X"));
 
   // Append button to li
   li.appendChild(deleteBtn);
-
-  // Create a arrow down Element
-
-  var arrowDown = document.createElement("span");
-
-  // Add class to arrow down
-
-  arrowDown.className = "material-icons";
-
-  // Append to li elements
-
-  li.appendChild(arrowDown);
 
   // Add li to ul
   taskList.appendChild(li);
