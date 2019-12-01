d46c7f5c69e166e9ea14558667da489326e56a55
Swastik Panda <swastikpanda32@gmail.com>
2019-12-01 22:30:56 +0530

    added arrowDown functionality,deleted span element, gave new class to arrowDown

--- script.js
+++ script.js
@@ -17,37 +17,37 @@ function addTask(e) {
 
   // Create new li element
   var li = document.createElement("li");
+
   // Add class
   li.className = "task-item";
 
   // Add text node with input value
   li.appendChild(document.createTextNode(newTask));
 
-  // Create del button element
+  // Create an <i> element for arrow down
+  var iElement = document.createElement("i");
 
+  // Add material icon class to it
+  iElement.className = "material-icons down";
+
+  // Add content to <i> tag so that it can dispaly icon
+  iElement.innerHTML = "keyboard_arrow_down";
+
+  // Append to li elements
+  li.appendChild(iElement);
+
+  // Moved the del button to bottom so that it can be appended after <span>
+  // Create del button element
   var deleteBtn = document.createElement("button");
 
   // ADD Class to del button
   deleteBtn.className = "btn-danger";
 
   // Append text node
-
   deleteBtn.appendChild(document.createTextNode("X"));
 
   // Append button to li
   li.appendChild(deleteBtn);
-
-  // Create a arrow down Element
-
-  var arrowDown = document.createElement("span");
-
-  // Add class to arrow down
-
-  arrowDown.className = "material-icons";
-
-  // Append to li elements
-
-  li.appendChild(arrowDown);
 
   // Add li to ul
   taskList.appendChild(li);
@@ -64,3 +64,16 @@ function removeItem(e) {
     // }, 1000);
   }
 }
+
+window.onload = function() {
+  var downArrow = document.querySelectorAll(".down");
+
+  for (var i = 0; i < downArrow.length; i++) {
+    downArrow[i].addEventListener("click", function() {
+      var addDown = this.parentElement;
+
+      if (addDown.nextElementSibling)
+        addDown.parentNode.insertBefore(addDown.nextElementSibling, addDown);
+    });
+  }
+};
